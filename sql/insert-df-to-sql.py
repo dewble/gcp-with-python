fb34a59f1a4728e34d345508f3bc0903b66f18e3
dewble <dewble62@gmail.com>
2022-06-13 18:00:17 +0900

    insert df to sql - 진행중

--- sql/insert-df-to-sql.py
+++ sql/insert-df-to-sql.py
@@ -1,3 +1,4 @@
+import sys
 import pymysql
 import pandas as pd
 import settings as st
@@ -14,15 +15,27 @@ try:
     )
 except pymysql.Error as e:
     print(f"Error connecting to mysql db: {e}")
+    sys.exit(1)
 
+
+# get cursor
 cur = conn.cursor()
 
+""" csv 파일로부터 dateframe 가져오기 """
+df = pd.read_csv("C:/Users/user/Desktop/workspace/python/gcp-with-python/sql/아파트(매매)__실거래가_20050901_20060831.csv", encoding="utf-8-sig", error_bad_lines=False)
+
+# print(df.head())
+# print(df.info())
+# print(df.describe())
+
+
+df.to_sql(name="real_estate", con=conn, index=False)
+
+
 # cur.execute("CREATE TABLE grade(name TEXT, phone TEXT, address TEXT)")
-cur.execute("select * from grade")
-conn.commit()
+cur.execute("select * from real_estate")
+# conn.commit()
 conn.close()
 
 
-""" csv 파일로부터 dateframe 가져오기 """
-# doc = pd.read_csv("C:/Users/user/Desktop/workspace/python/gcp-with-python/sql/아파트(매매)__실거래가_20050901_20060831.csv", encoding="utf-8-sig", error_bad_lines=False)
 
