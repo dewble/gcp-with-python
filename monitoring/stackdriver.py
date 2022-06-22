85d23d394a32d6712322a1e42dfa98ccd65aa421
dewble <dewble62@gmail.com>
2022-06-22 14:57:55 +0900

    monitor gcp with stackdriver

--- monitoring/stackdriver.py
+++ monitoring/stackdriver.py
@@ -1,9 +1,15 @@
 from google.cloud import monitoring_v3
 import time
+import os
+import settings as st
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]= st.SERVICE_KEY_P_2
+
 
 
 client = monitoring_v3.MetricServiceClient()
-project = 'jmhan-int-220517'  # TODO: Update to your project ID.
+# project = st.P_1  # TODO: Update to your project ID.
+project = st.P_2  # TODO: Update to your project ID.
 project_name = f"projects/{project}"
 
 
@@ -20,10 +26,10 @@ project_name = f"projects/{project}"
 
 
 """모니터링 리소스 세부정보 가져오기"""
-resource_path = (
-    f"projects/{project}/monitoredResourceDescriptors/gce_instance"
-)
-print(client.get_monitored_resource_descriptor(name=resource_path))
+# resource_path = (
+#     f"projects/{project}/monitoredResourceDescriptors/gce_instance"
+# )
+# print(client.get_monitored_resource_descriptor(name=resource_path))
 
 
 
@@ -51,25 +57,25 @@ print(client.get_monitored_resource_descriptor(name=resource_path))
 
 
 """Metric값 가져오기"""
-# interval = monitoring_v3.TimeInterval()
-#
-# now = time.time()
-# seconds = int(now)
-# nanos = int((now - seconds) * 10 ** 9)
-# interval = monitoring_v3.TimeInterval(
-#     {
-#         "end_time": {"seconds": seconds, "nanos": nanos},
-#         "start_time": {"seconds": (seconds - 1200), "nanos": nanos},
-#     }
-# )
-#
-# results = client.list_time_series(
-#     request={
-#         "name": project_name,
-#         "filter": 'metric.type = "compute.googleapis.com/instance/cpu/utilization"',
-#         "interval": interval,
-#         "view": monitoring_v3.ListTimeSeriesRequest.TimeSeriesView.FULL,
-#     }
-# )
-# for result in results:
-#     print(result)
+interval = monitoring_v3.TimeInterval()
+
+now = time.time()
+seconds = int(now)
+nanos = int((now - seconds) * 10 ** 9)
+interval = monitoring_v3.TimeInterval(
+    {
+        "end_time": {"seconds": seconds, "nanos": nanos},
+        "start_time": {"seconds": (seconds - 1200), "nanos": nanos},
+    }
+)
+
+results = client.list_time_series(
+    request={
+        "name": project_name,
+        "filter": 'metric.type = "compute.googleapis.com/instance/cpu/utilization"',
+        "interval": interval,
+        "view": monitoring_v3.ListTimeSeriesRequest.TimeSeriesView.FULL,
+    }
+)
+for result in results:
+    print(result)
